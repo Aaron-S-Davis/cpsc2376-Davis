6ce9aa47b1182a9565695b22a2a4dad7580b3b08
Aaron-S-Davis <aarondavis114@gmail.com>
2025-02-25 16:40:28 -0600

    Add files via upload

--- projects/project01/main.cpp
+++ projects/project01/main.cpp
@@ -2,17 +2,25 @@
 
 int main() {
 
-	std::vector<std::vector<char>> gameBoard(6, std::vector<char>(7, ' '));
+	
 	int playerTurn = 0;
+	std::string prompt;
 	ConnectFour game;
 	GameState state = GameState::ONGOING;
 
 	while (true) {
 		std::cout << "Connect Four: Drop pieces in columns to make four in a row.\nCan win horizontally, vertically or diagonally\n";
+		std::vector<std::vector<char>> gameBoard(6, std::vector<char>(7, ' '));
 
 		while (state == GameState::ONGOING) {
+			if (playerTurn % 2 == 0) {
+				prompt = "Choose a column 1-7 (X): ";
+			}
+			else if (playerTurn % 2 == 1) {
+				prompt = "Choose a column 1-7 (O): ";
+			}
 			game.makeBoard(gameBoard);
-			game.play(gameBoard, game.getInput("Choose a column 1-7: "), playerTurn);
+			game.play(gameBoard, game.getInput(prompt), playerTurn);
 			state = game.gameStatus(gameBoard);
 		}
 
@@ -26,7 +34,17 @@ int main() {
 		else {
 			std::cout << "It's a draw!\n";
 		}
-		return 0;
+
+		int choice = game.getInput("1. Play again!\n0. Exit\n");
+		if (choice == 1) {
+			std::cout << "Restarting...\n";
+			gameBoard.clear();
+			state = GameState::ONGOING;
+			continue;
+		}
+		else if (choice == 0) {
+			return 0;
+		}
 	}
 
 	return 0;
