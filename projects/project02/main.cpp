b4d23dedcdd76dc12622069e3192dd1d5571c9df
Aaron-S-Davis <aarondavis114@gmail.com>
2025-03-30 22:10:23 -0500

    Update main.cpp

--- projects/project02/main.cpp
+++ projects/project02/main.cpp
@@ -4,12 +4,12 @@ int getInput(std::string prompt);
 
 int main() {
 
-	std::string prompt;
-
 	while (true) {
 		std::cout << "Connect Four: Drop pieces in columns to make four in a row.\nCan win horizontally, vertically or diagonally\n";
 
+		//creates new game pointer with blank gameboard
 		Game* game = new Game;
+		std::string prompt;
 
 		while (game->status() == game->ONGOING) {
 			if (game->turn() == game->P1_TURN) {
@@ -18,11 +18,14 @@ int main() {
 			else if (game->turn() == game->P2_TURN) {
 				prompt = "Choose a column 1-7 (O): ";
 			}
+			//output gameboard after every turn
 			std::cout << *game;
 			game->play(getInput(prompt) - 1);
 		}
 
+		//shows final gameboard
 		std::cout << *game;
+		//checks game status function for win condition
 		if (game->status() == game->PLAYER_1_WINS) {
 			std::cout << "Player 1 (X) wins!\n";
 		}
@@ -34,6 +37,7 @@ int main() {
 		}
 
 		int choice = getInput("1. Play again!\n0. Exit\n");
+		//deletes current game so a new one can be created
 		delete game;
 		if (choice == 1) {
 			std::cout << "Restarting...\n";
@@ -47,6 +51,7 @@ int main() {
 	return 0;
 }
 
+//basic input validation for integer input
 int getInput(std::string prompt) {
 	std::cout << prompt;
 	int input;
